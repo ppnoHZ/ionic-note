b623127ce1fba64a45aface4ca89835e2d9fa880
zhoudd <zdd_910@126.com>
2015-11-18 00:46:48 +0800

    发现 ion-content 会创建一个$scope作用域。

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,9 +7,9 @@ angular.module('starter.controllers', [])
     $scope.register= function () {
       console.log($scope)
       var data={
-        username:$scope.username,
-        nickname:$scope.nickname,
-        password:$scope.password
+        username:$scope.userInfo.username,
+        nickname:$scope.userInfo.nickname,
+        password:$scope.userInfo.password
       };
       console.log(data)
       $http.post('192.168.3.105:18080/account/signup',data
